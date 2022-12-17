f94dbf7a9eaf246965325d0c2bf0ec0357654e41
AlbArtAmpAn <120706248+AlbArtAmpAn@users.noreply.github.com>
2022-12-18 00:47:59 +0700

    Update EditKntl.lua

--- EditKntl.lua
+++ EditKntl.lua
@@ -1,9 +1,1 @@
 MatikanBot = false
-
----===== Info World Pack =====---
-autobuypack = true
-hargapack = 20000 -- HARGA PACK
-namapack = "world_lock_10_pack" -- NAMA PACK
-MinItemPack = 15 --APABILA JUMLAH ITEM YANG ADA DIDALAM iditempack LEBIH BESAR DARIPADA MinItemPack, MAKA BOTNYA MULAI DROP PACKNYA KE STORAGE!
-maxbuy = 10 --MAKSIMAL BOTNYA AKAN REPEAT BELI PACK JIKA GEMSNYA MASIH CUKUP!
-iditempack = {242}
