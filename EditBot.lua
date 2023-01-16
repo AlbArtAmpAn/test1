55cd0ee26992a0f82ab2f9868bf45cb0f19a14f2
AlbArtAmpAn <120706248+AlbArtAmpAn@users.noreply.github.com>
2023-01-16 14:24:27 +0700

    Update EditBot.lua

--- EditBot.lua
+++ EditBot.lua
@@ -4,3 +4,15 @@ namapack = "mega_party_pack" -- NAMA PACK
 MinItemPack = 5 --APABILA JUMLAH ITEM YANG ADA DIDALAM iditempack LEBIH BESAR DARIPADA MinItemPack, MAKA BOTNYA MULAI DROP PACKNYA KE STORAGE!
 maxbuy = 10 --MAKSIMAL BOTNYA AKAN REPEAT BELI PACK JIKA GEMSNYA MASIH CUKUP!
 iditempack = {2306, 7672}
+
+if getPing() < 150 then
+    delayht = 75 -- DELAY HARVEST
+    delayplant = 50 -- DELAY PLANt
+    delaypnb = 165 -- DELAY PNB
+    delayplace = 115 -- DELAY PLACE
+else
+    delayht = 100 -- DELAY HARVEST
+    delayplant = 75 -- DELAY PLANT
+    delaypnb = 185 -- DELAY PNB
+    delayplace = 125 -- DELAY PLACE
+end
