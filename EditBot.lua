098326fec0b09e6bf7610cbda53226193d8c58cd
AlbArtAmpAn <120706248+AlbArtAmpAn@users.noreply.github.com>
2023-01-16 14:25:59 +0700

    Update EditBot.lua

--- EditBot.lua
+++ EditBot.lua
@@ -1,8 +1,8 @@
 autobuypack = true
-hargapack = 50000 -- HARGA PACK
-namapack = "mega_party_pack" -- NAMA PACK
-MinItemPack = 5 --APABILA JUMLAH ITEM YANG ADA DIDALAM iditempack LEBIH BESAR DARIPADA MinItemPack, MAKA BOTNYA MULAI DROP PACKNYA KE STORAGE!
-maxbuy = 10 --MAKSIMAL BOTNYA AKAN REPEAT BELI PACK JIKA GEMSNYA MASIH CUKUP!
+hargapack = 50000
+namapack = "mega_party_pack"
+MinItemPack = 5
+maxbuy = 10
 iditempack = {2306, 7672}
 
 if getPing() < 150 then
