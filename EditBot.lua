5711a1973fbe5942e3942e7e476356870bff2d29
AlbArtAmpAn <120706248+AlbArtAmpAn@users.noreply.github.com>
2023-01-18 12:07:47 +0700

    Update EditBot.lua

--- EditBot.lua
+++ EditBot.lua
@@ -5,17 +5,10 @@ MinItemPack = 15 --APABILA JUMLAH ITEM YANG ADA DIDALAM iditempack LEBIH BESAR D
 maxbuy = 5 --MAKSIMAL BOTNYA AKAN REPEAT BELI PACK JIKA GEMSNYA MASIH CUKUP!
 iditempack = {242, 2306, 7672}
 
-if getPing() < 150 then
-    delayht = 75 -- DELAY HARVEST
-    delayplant = 50 -- DELAY PLANt
-    delaypnb = 165 -- DELAY PNB
-    delayplace = 115 -- DELAY PLACE
-else
-    delayht = 100 -- DELAY HARVEST
-    delayplant = 75 -- DELAY PLANT
-    delaypnb = 185 -- DELAY PNB
-    delayplace = 125 -- DELAY PLACE
-end
+delayht = 70 -- DELAY HARVEST
+delayplant = 50 -- DELAY PLANT
+delaypnb = 165 -- DELAY PNB
+delayplace = 115 -- DELAY PLACE
 
 JamSkrg = 8
 wkt = os.date("*t", os.time())
