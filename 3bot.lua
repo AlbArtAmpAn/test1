664d53625c3a62dd44204dfb8b809c1ef60e290f
AlbArtAmpAn <120706248+AlbArtAmpAn@users.noreply.github.com>
2023-02-08 08:26:04 +0700

    Update 3bot.lua

--- 3bot.lua
+++ 3bot.lua
@@ -23,7 +23,7 @@ WorldSeed = {"SA27SH", "AH17AHQ"}
 
 WorldPack = {"18AJA2", "16AKA11", "AHA11AJ"}
 
-JamSkrg = 6
+JamSkrg = 8
 wkt = os.date("*t", os.time())
 if JamSkrg >= wkt.hour then
     Selisih = (JamSkrg  - wkt.hour)
